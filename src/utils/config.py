44a98669c9bb976489f71a96f716ff7254c4134f
DKowalski25 <dshinoda25@gmail.com>
2024-03-25 19:39:28 +0700

    PB-60 Add to config data for DB

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,4 +1,5 @@
 import os
+from typing import Final
 
 from dotenv import load_dotenv
 
@@ -19,3 +20,14 @@ def get_env_var(var_name: str) -> str:
     else:
         return value
 
+
+APP_HOST: Final[str] = get_env_var("APP_HOST")
+KAFKA_HOST: Final[str] = get_env_var("KAFKA_HOST")
+APP_PORT: Final[int] = int(get_env_var("APP_PORT"))
+KAFKA_PORT: Final[int] = int(get_env_var("KAFKA_PORT"))
+KAFKA_TOPIC_NAME: Final[str] = get_env_var("KAFKA_TOPIC_NAME")
+USERNAME_DB: Final[str] = get_env_var("USERNAME_DB")
+HOST_DB: Final[str] = get_env_var("HOST_DB")
+PASSWORD_DB: Final[str] = get_env_var("PASSWORD_DB")
+DATABASE_NAME: Final[str] = get_env_var("DATABASE_NAME")
+PORT_DB: Final[str] = get_env_var("PORT_DB")
