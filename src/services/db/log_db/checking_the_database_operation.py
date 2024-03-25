b688c3dd06d97b40423d793c6cae70fc2bb22056
DKowalski25 <dshinoda25@gmail.com>
2024-03-25 20:21:39 +0700

    PB-60 Completely rewrote the file

--- src/services/db/log_db/checking_the_database_operation.py
+++ src/services/db/log_db/checking_the_database_operation.py
@@ -1,25 +1,33 @@
 import asyncio
+import datetime
 
-from sqlalchemy import URL
+from src.services.db.log_db.PostgreAPI import SQLAlchemyDatabase
+from src.utils.config import DB_URL
 
-from src.services.db.log_db.PostgreAPI import SQLAlchemyDatabase, url_p
+
+async def main():
+    db = SQLAlchemyDatabase(DB_URL)
+
+    # Данные для вставки в базу данных
+    log_data = {
+        "app_name": "my_app",
+        "module": "new",
+        "level": "INFO",
+        "timestamp": datetime.datetime.strptime(
+            "2024-03-20 12:00:00", "%Y-%m-%d %H:%M:%S"
+        ),
+        "message": "This is a test log message.",
+    }
+
+    # Вставляем запись в базу данных
+    await db.insert_log(log_data)
+
+    # Получаем все записи из базы данных для модуля "my_module"
+    logs = await db.get_log_by_model("my_module")
+
+    # Выводим полученные записи
+    print(logs)
 
 
-db = SQLAlchemyDatabase(url_p)
-# Данные для вставки в базу данных
-log_data = {
-    "app_name": "my_app",
-    "module": "my_module",
-    "level": "DEBUG",
-    "timestamp": "2024-03-20 12:00:00",
-    "message": "This is a test log message."
-}
-
-# Вставляем запись в базу данных
-asyncio.run(db.insert_log(log_data))
-
-# Получаем все записи из базы данных для модуля "my_module"
-logs = asyncio.run(db.get_log_by_model("my_module"))
-
-# Выводим полученные записи
-print(logs)
+if __name__ == "__main__":
+    asyncio.run(main())
